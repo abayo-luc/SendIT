d5402e9d817141d9be3fcb101f99dc07aa2416b3
Luc.A <luc.bayo@gmail.com>
2018-11-08 16:28:21 +0200

    remove body-parcer and used express-json

--- src/server.js
+++ src/server.js
@@ -1,21 +1,17 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 // load all routes
 import routers from './routes/api/v1';
 
 const app = express();
 // body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 // use all routers
 app.use('/api/v1/', routers);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(
-    `Server started on ${port}, visit Visit: http://localhost:${port}`
-  );
+  console.log(`Server started on ${port}`);
 });
 
 // exposing the sever for testing
