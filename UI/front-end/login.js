f47f8d3ec4667a5c63b8db0cb6382c8bfdef7b61
abayo-luc <luc.bayo@gmail.com>
2018-11-27 10:38:11 +0200

    [ft-front-end-login-#162213875]Add login functionality

--- UI/front-end/login.js
+++ UI/front-end/login.js
@@ -2,21 +2,62 @@ const loginEmail = document.querySelector("#login-email");
 const loginPassword = document.querySelector(
   "#login-password"
 );
+const authError = document.querySelector("#auth-error");
 const loginButton = document.querySelector("#login-button");
 const login = () => {
+  cleanError();
   const data = {
     email: loginEmail.value,
     password: loginPassword.value
   };
 
-  axios
-    .post("/api/v1/login", data)
-    .then(async response => {
-      const { data } = response;
-      await localStorage.setItem("token", data.token);
-      window.location = "../pages/ClientDashboard.html";
+  fetch("/api/v1/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "Application/JSON"
+    }
+  })
+    .then(response => {
+      response.json().then(async result => {
+        const { token, message, errors } = result;
+        if (token) {
+          await localStorage.setItem("token", result.token);
+          window.location = "../pages/ClientDashboard.html";
+          return;
+        } else if (message) {
+          authError.innerHTML = `<p>${message}</p>`;
+          authError.style.display = "block";
+          return;
+        } else if (errors) {
+          // if (errors.email)
+          loginEmail.parentElement.querySelector(
+            ".errors"
+          ).innerHTML = `<p>${errors.email}</p>`;
+          loginPassword.parentElement.querySelector(
+            ".errors"
+          ).innerHTML = `<p>${errors.password}</p>`;
+          return;
+        }
+        return;
+      });
     })
-    .catch(err => console.log(err.response));
+    .catch(err => {
+      err.json().then(result => {
+        const { message } = result;
+        console.log(message);
+      });
+    });
 };
 
+const cleanError = () => {
+  authError.innerHTML = "";
+  loginEmail.parentElement.querySelector(
+    ".errors"
+  ).innerHTML = "";
+  loginPassword.parentElement.querySelector(
+    ".errors"
+  ).innerHTML = "";
+  return;
+};
 loginButton.addEventListener("click", login);
